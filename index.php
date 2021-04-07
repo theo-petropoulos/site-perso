9aa2ba9191dfe94d8cfb8dc5f2d3a718b89cbf94
Theo <theo.petropoulos@laplateforme.io>
2021-04-07 23:24:54 +0200

    test meta og

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
         <meta http-equiv="expires" content="86400">
         <meta name="description" content="Petropoulos Théo, développeur web & web mobile à Marseille. En recherche d'alternance.">
         <meta name="keywords" content="Petropoulos, Théo, Développeur, Web, Developer, Marseille, Alternance, Javascript, Jquery, JS, PHP, HTML, CSS">
+        <meta property="og:image" content="assets/images/other/og.png">
         <link rel="stylesheet" href="<?=$addr;?>css/theo.petropoulos.css?v=<?php echo time(); ?>">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet"> 
